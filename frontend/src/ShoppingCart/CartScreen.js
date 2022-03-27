5e138e07c3a8fee834b4d988495386d1643c90cd
Lori <57802869+loriZ77@users.noreply.github.com>
2022-03-26 22:02:56 -0700

    Update Cart page UI with dummy image, title, price and summary col

--- frontend/src/ShoppingCart/CartScreen.js
+++ frontend/src/ShoppingCart/CartScreen.js
@@ -2,6 +2,8 @@ import React, {useState} from "react";
 import Navigation from "../Navigation";
 import styled from "styled-components";
 
+import items from "../DummyData/dummyBooks.json";
+
 const Wrapper = styled.div` 
  padding: 20px;
  `;
@@ -23,7 +25,7 @@ const TopButton = styled.button`
     font-weight: 600;
     cursor: pointer;
     border: ${props=>props.type === "filled" && "none"};
-    background-color: ${(props)=>props.type === "filled" ? "black" : "transparent"};
+    background-color: ${(props)=>props.type === "filled" ? "#98bae7" : "transparent"};
     color: ${props=>props.type === "filled" && "white"};
 `;
 
@@ -33,11 +35,64 @@ const TopText = styled.span`
     margin: 0px 10px;
 `;
 
+const Bottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Info = styled.div`
+    flex: 3;
+`;
+
+const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
+
+`;
+
+const Summary = styled.div`
+    flex: 1;
+`;
+
+const ProductDetail = styled.div`
+    flex: 2;
+    display: flex;
+`;
+
+const Image = styled.img`
+    width: 200px;
+    height: 300px;
+`;
+
+const Details = styled.div`
+    padding: 20px;
+    display: flex;
+    
+`;
+const ProductName = styled.span``;
+const PriceDetail = styled.div`
+    flex: 1;
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const ProductAmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+`;
+const ProductAmount = styled.div`
+    font=size: 24px;
+    margin: 5px
+`;
+const ProductPrice = styled.div``;
+
 const CartScreen = () => {
     return (
         <div>
             <Navigation/>
-            <Wrapper/>
+            <Wrapper>
                 <Title>Your Shopping Cart</Title>
                 <Top>
                     <TopButton>Continue Shopping</TopButton>
@@ -46,8 +101,33 @@ const CartScreen = () => {
                         <TopText>Wish List(1)</TopText>
                     </div>
                     <TopButton type="filled">Check Out Now! </TopButton>
-
                 </Top>
+
+                <Bottom>
+                    <Info>
+                        <Product>
+                            <ProductDetail>
+                                <Image src={items[0].volumeInfo.imageLinks.small}></Image>
+                                <Details>
+                                    <ProductName><b>The Google Story</b></ProductName>
+
+                                </Details>
+                            </ProductDetail>
+                            <PriceDetail>
+                                <ProductAmountContainer>
+                                    {/*<Add/>*/}
+                                    <ProductAmount>2</ProductAmount>
+                                    {/*<Remove/>*/}
+                                </ProductAmountContainer>
+                                <ProductPrice>$ {items[0].saleInfo.listPrice.amount}</ProductPrice>
+                            </PriceDetail>
+                        </Product>
+                    </Info>
+                    <Summary>
+                        Summary
+                    </Summary>
+                </Bottom>
+            </Wrapper>
         </div>
     )
 }
