35aadeebcb17f575889cb9cd17592ad66667e652
Yuyan Zhou <57802869+loriZ77@users.noreply.github.com>
2022-04-07 20:32:39 -0700

    add /auth path ub App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,21 +6,20 @@ import ResultsScreen from "./ResultsScreen";
 import DetailsScreen from "./DetailsScreen";
 import LoginScreen from "./Authentication/LoginScreen";
 import SignUpScreen from "./Authentication/SignUpScreen";
+import CartScreen from "./ShoppingCart/CartScreen";
 import {useEffect, useState} from "react";
 
-function App() {
-  const [user, setUser] = useState();
-  const [flag, refreshPage] = useState(true);
-  useEffect(() => {
-    getUser().then((user) => {
-      setUser(user);
-      console.log(`re-render user: ${user.username}`);
-    });
-  }, [flag]);
-
-import CartScreen from "./ShoppingCart/CartScreen";
 
 function App() {
+    const [user, setUser] = useState();
+    const [flag, refreshPage] = useState(true);
+    useEffect(() => {
+        getUser().then((user) => {
+            setUser(user);
+            console.log(`re-render user: ${user.username}`);
+        });
+    }, [flag]);
+
   return (
     <div className="container-fluid">
         <BrowserRouter>
@@ -28,8 +27,8 @@ function App() {
                 <Route path="/" element={<HomeScreen/>}></Route>
                 <Route path="/results/:keyword" element={<ResultsScreen/>}></Route>
                 <Route path="/details/:id" element={<DetailsScreen/>}></Route>
-                <Route exact path="/login" element={<LoginScreen/>}></Route>
-                <Route exact path="/signup" element={<SignUpScreen/>}></Route>
+                <Route exact path="/auth/login" element={<LoginScreen/>}></Route>
+                <Route exact path="/auth/signup" element={<SignUpScreen/>}></Route>
                 <Route exact path="/shopping_cart" element={<CartScreen/>}></Route>
 
             </Routes>
