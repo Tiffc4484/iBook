e9a01b8398126a72cbe634034d1dd06bcf0e7c37
Yuyan Zhou <57802869+loriZ77@users.noreply.github.com>
2022-04-09 23:59:09 -0700

    fix authentication, update two cart services

--- frontend/src/ShoppingCart/service/cart-service.js
+++ frontend/src/ShoppingCart/service/cart-service.js
@@ -1,35 +1,26 @@
-const CART_URL = "http://localhost:3001/cart"
-const USERS_URL = "http://localhost:3001/api/users"
+const LOCALHOST_URL = "http://localhost:3001"
+const USERS_URL = "http://localhost:3001/auth/user"
 const BOOKS_API = "https://ibook5500.herokuapp.com/api/books";
 
 
 const addBookToCart = (username, bookId) =>
-    fetch(`${CART_URL}/${username}/${bookId}`, {
+    fetch(`${LOCALHOST_URL}/${username}/cart`, {
         method: 'POST',
-        body: JSON.stringify(username),
-        headers: {
-            'content-type': 'application/json'
-        }
-    }).then(response => response.json())
-
-const addFavoriteToUser = (username, mealId) =>
-    fetch(`${CART_URL}/${username}`, {
-        method: 'POST',
-        body: JSON.stringify(mealId),
+        body: JSON.stringify(bookId),
         headers: {
             'content-type': 'application/json'
         }
     }).then(response => response.json())
 
 
-const findUsersForFavorite = (mealId) => {
-    fetch(`${USERS_URL}/favorites/${mealId}`)
+
+const findAllBooksInCart = (username) => {
+    fetch(`${LOCALHOST_URL}/${username}/cart`)
         .then(response => response.json())
 }
 
 
 export default {
     addBookToCart,
-    addFavoriteToUser,
-    findUsersForFavorite
+    findAllBooksInCart
 }
